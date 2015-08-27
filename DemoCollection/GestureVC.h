a0f9677d3ea3115326764ed82601dea35e1681e2
trier <23625272612@qq.com>
2015-08-27 19:47:07 +0800

    是的撒

--- DemoCollection/GestureVC.h
+++ DemoCollection/GestureVC.h
@@ -8,6 +8,18 @@
 
 #import <UIKit/UIKit.h>
 
+typedef enum {
+    DirectionUp = 0,
+    DirectionDown = 1,
+    DirectionLeft = 2,
+    DirectionRight = 3,
+    DirectionLeftUp= 4,
+    DirectionLeftDown = 5,
+    DirectionRightUp = 6,
+    DirectionRightDown = 7
+} Direction;  //云台方向
+
+
 @interface GestureVC : UIViewController
 @property (strong, nonatomic) IBOutlet UIView *mView;
 
